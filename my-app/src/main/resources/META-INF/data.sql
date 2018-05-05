0247c4fb26c77432c7a71960cbe7c65068b80fb1
Jerzy Kiryczuk <jurkirk@gmail.com>
2018-05-05 17:25:15 +0200

    Basic controller + sql not working

---
+++ my-app/src/main/resources/META-INF/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO cities(id,name_of_city) VALUES (1,'Łódź');
+INSERT INTO cities(id,name_of_city) VALUES (2,'Poznań');
+
+--
+-- INSERT INTO cinemas (id,address,cities_id_cities) VALUES (1,'Drewnowska 58',1);
+-- INSERT INTO cinemas (id,address,cities_id_cities) VALUES (2,'Dróżbieckiego 2',2);
+--
+--
+-- INSERT INTO movies(id,actors,director,duration,premiere_date,title) VALUES (1,'Alden Ehrenreich, Donald Glover, Emilia Clarke, Woody Harrelson, Paul Bettany, Warwick Davis','Ron Howard',135,'2018-05-28','Han Solo: A Star Wars Story')
+--
